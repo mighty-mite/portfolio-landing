a1b3272c3d9234013ca363dddc33f6008f9a574a
Ayur Tushinov <tushinov.ayur@gmail.com>
2024-09-06 19:09:15 +0300

    feat: add projects

--- js/index.js
+++ js/index.js
@@ -38,3 +38,163 @@ icons.map((item) => {
   listItem.append(img);
   list.append(listItem);
 });
+
+// CARDS
+
+function createElement(
+  tag,
+  className,
+  attributeName,
+  attribute,
+  textContent,
+  parent
+) {
+  const element = document.createElement(tag);
+  if (className) element.classList.add(className);
+  if (attributeName && attribute)
+    element.setAttribute(attributeName, attribute);
+  if (textContent && typeof textContent === "string") {
+    if (document.body.clientWidth >= 1280) {
+      element.textContent = textContent;
+    } else if (document.body.clientWidth < 1280 && textContent.length > 30) {
+      const trimmed = textContent.slice(0, 30) + "...";
+      element.textContent = trimmed;
+    } else {
+      element.textContent = textContent;
+    }
+  }
+  if (parent) parent.append(element);
+
+  return element;
+}
+
+const text =
+  "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content";
+/* Rectangle 1 */
+
+const cardBkgs = [
+  "./../card-bkg/1.png",
+  "./../card-bkg/2.png",
+  "./../card-bkg/3.png",
+  "./../card-bkg/4.png",
+  "./../card-bkg/5.png",
+  "./../card-bkg/6.png",
+];
+
+const cardList = document.querySelector(".projects__list");
+
+cardBkgs.map((item) => {
+  const li = createElement("li", "card", null, null, null, cardList);
+  const img = createElement("img", "card__img", "src", item, null, li);
+  const contentContainer = createElement(
+    "div",
+    "card__content",
+    null,
+    null,
+    null,
+    li
+  );
+  const heading = createElement(
+    "h3",
+    "card__heading",
+    null,
+    null,
+    "Project tile goes here",
+    contentContainer
+  );
+  const description = createElement(
+    "p",
+    "card__description",
+    null,
+    null,
+    text,
+    contentContainer
+  );
+
+  const stackContainer = createElement(
+    "div",
+    "card__stack-container",
+    null,
+    null,
+    null,
+    contentContainer
+  );
+  const heading2 = createElement(
+    "h3",
+    "card__heading-2",
+    null,
+    null,
+    "Tech Stack: ",
+    stackContainer
+  );
+  const stack = createElement(
+    "p",
+    "card__stack",
+    null,
+    null,
+    "HTML , JavaScript, SASS, React",
+    stackContainer
+  );
+
+  const linksContainer = createElement(
+    "div",
+    "card__links",
+    null,
+    null,
+    null,
+    contentContainer
+  );
+  const liveLink = createElement(
+    "a",
+    "card__live",
+    "href",
+    "#",
+    null,
+    linksContainer
+  );
+  const liveImg = createElement(
+    "img",
+    "card__live-img",
+    "src",
+    "./img/link.svg",
+    null,
+    liveLink
+  );
+  const liveText = createElement("span", "", "", "", "Live Preview", liveLink);
+
+  const githubLink = createElement(
+    "a",
+    "card__github",
+    "href",
+    "#",
+    null,
+    linksContainer
+  );
+  const githubImg = createElement(
+    "img",
+    "card__github-img",
+    "src",
+    "./img/github-black.svg",
+    null,
+    githubLink
+  );
+  const githubText = createElement(
+    "span",
+    "",
+    "",
+    "",
+    "Code Preview",
+    githubLink
+  );
+});
+
+window.addEventListener("resize", (e) => {
+  const width = document.body.clientWidth;
+  const descriptions = document.querySelectorAll(".card__description");
+  descriptions.forEach((item) => {
+    if (width < 1280) {
+      const trimmed = text.slice(0, 30) + "...";
+      item.textContent = trimmed;
+    }
+  });
+});
