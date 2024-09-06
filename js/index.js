843be2a0146d5abd113f3b59704c6a4aec79069c
Ayur Tushinov <tushinov.ayur@gmail.com>
2024-09-06 11:08:24 +0300

    feat: add stack

--- js/index.js
+++ js/index.js
@@ -10,3 +10,31 @@ menuBtn.addEventListener("click", () => {
     menuBox.style.right = "-100%";
   }
 });
+
+// STACK
+
+const icons = [
+  "./../icons/html.svg",
+  "./../icons/css.svg",
+  "./../icons/js.svg",
+  "./../icons/react.svg",
+  "./../icons/redux.svg",
+  "./../icons/bootstrap.svg",
+  "./../icons/tailwind.svg",
+  "./../icons/sass.svg",
+  "./../icons/git.svg",
+  "./../icons/guy.svg",
+  "./../icons/vscode.svg",
+  "./../icons/github.svg",
+];
+const list = document.querySelector(".stack__list");
+
+icons.map((item) => {
+  const listItem = document.createElement("li");
+  const img = document.createElement("img");
+  img.classList.add("stack__img");
+  img.setAttribute("src", item);
+  listItem.classList.add("stack__item");
+  listItem.append(img);
+  list.append(listItem);
+});
